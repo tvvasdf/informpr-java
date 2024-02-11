936b5f6e39103a597c5483ceaeb581140d515fe7
Vladimir <jasonstadzam@gmail.com>
2024-02-11 20:26:50 +0300

    first commit: spring boot files

---
+++ src/test/java/ru/informpr/cartridges/CartridgesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.informpr.cartridges;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartridgesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
