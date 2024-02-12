cb664aa243039393e125d671652f3282a5eaa5f9
Vladimir <jasonstadzam@gmail.com>
2024-02-13 00:22:24 +0300

    renamed StaticController to PagesController

--- src/main/java/ru/informpr/cartridges/controllers/PagesController.java
+++ src/main/java/ru/informpr/cartridges/controllers/PagesController.java
@@ -3,9 +3,17 @@ package ru.informpr.cartridges.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class StaticController {
+public class PagesController {
+
+    @GetMapping("/")
+    public static String mainPage(Model model, @RequestParam String q) {
+        //search for q in cartridges table
+        model.addAttribute("title", "Главная");
+        return "main";
+    }
     @GetMapping({"/about-printers", "/about-printers/"})
     public static String aboutPrinters(Model model) {
         model.addAttribute("title", "О принтерах");
