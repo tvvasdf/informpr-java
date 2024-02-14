1dcd2d4e0fcc2dab5d99e185350ace9cff02d409
Vladimir <jasonstadzam@gmail.com>
2024-02-14 23:05:26 +0300

    added new search methods in CartridgesRepository

--- src/main/java/ru/informpr/cartridges/repo/CartridgesRepository.java
+++ src/main/java/ru/informpr/cartridges/repo/CartridgesRepository.java
@@ -1,6 +1,12 @@
 package ru.informpr.cartridges.repo;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import ru.informpr.cartridges.models.Cartridges;
 
+import java.util.List;
+
 public interface CartridgesRepository extends CrudRepository<Cartridges, Integer> {
+    List<Cartridges> findByModelOrPrefixOrPrintersContaining(String model, String prefix, String printers);
+    List<Cartridges> findByModelOrPrefixOrPrintersOrPropertiesContaining(String model, String prefix, String printers, String properties);
+
 }
