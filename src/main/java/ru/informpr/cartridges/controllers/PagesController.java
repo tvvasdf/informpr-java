139f0ac5f9a9039739874b707dc392c7825fba11
Vladimir <jasonstadzam@gmail.com>
2024-02-14 23:06:01 +0300

    added search page

--- src/main/java/ru/informpr/cartridges/controllers/PagesController.java
+++ src/main/java/ru/informpr/cartridges/controllers/PagesController.java
@@ -1,19 +1,49 @@
 package ru.informpr.cartridges.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import ru.informpr.cartridges.models.Cartridges;
+import ru.informpr.cartridges.repo.CartridgesRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @Controller
 public class PagesController {
 
+    @Autowired
+    private CartridgesRepository cartridgesRepo;
+
     @GetMapping("/")
-    public static String mainPage(Model model, @RequestParam String q) {
-        //search for q in cartridges table
-        model.addAttribute("title", "Главная");
+    public String mainPage(
+        Model model,
+       @RequestParam(required = false, defaultValue = "") String q,
+       @RequestParam(required = false, defaultValue = "") String props
+    ) {
+        if (!q.isEmpty()) {
+            List<Cartridges> cartridges;
+
+            if (Objects.equals(props, "Y")) {
+                cartridges = cartridgesRepo.findByModelOrPrefixOrPrintersOrPropertiesContaining(q, q, q, q);
+            } else {
+                cartridges = cartridgesRepo.findByModelOrPrefixOrPrintersContaining(q, q, q);
+            }
+
+            model.addAttribute("title", "Поиск - " + q);
+            model.addAttribute("query", q);
+            model.addAttribute("props", props);
+            model.addAttribute("cartridges", cartridges);
+        } else {
+            model.addAttribute("title", "Главная");
+        }
+
         return "main";
     }
+
     @GetMapping({"/about-printers", "/about-printers/"})
     public static String aboutPrinters(Model model) {
         model.addAttribute("title", "О принтерах");
